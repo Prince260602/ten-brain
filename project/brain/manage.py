1b43a5e98a4a4b50f1d76302ddfc10f43e2eeb79
mounikavemulapalli <124512238+mounikavemulapalli@users.noreply.github.com>
2024-12-21 23:30:10 +0530

    reverted manage.py

--- project/brain/manage.py
+++ project/brain/manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brain.settings')
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ten-brain.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brain.settings')
+       
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
