6697c78ebad38686385f3364bcc749145b92da29
mounikavemulapalli <124512238+mounikavemulapalli@users.noreply.github.com>
2024-12-23 20:34:56 +0530

    Update settings.py

--- project/brain/brain/settings.py
+++ project/brain/brain/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+PORT = os.environ.get('PORT', '8000')
 
 # """
 # Django settings for brain project.
